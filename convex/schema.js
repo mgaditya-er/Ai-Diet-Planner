1c0da5a11eb3de7ac6949125f6969e78efe8ae4d
mgadity-er <adityamghyar@gmail.com>
2025-04-21 02:03:36 +0530

    setup: backend - convex| user schema create

---
+++ convex/schema.js
@@ -0,0 +1,13 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    Users:defineTable({
+        name:v.string(),
+        email:v.string(),
+        picture:v.string(),
+        subscriptionId:v.optional(v.string()),
+        credits:v.number()
+
+    })
+})
